25a72f8f7c771aef7c87464345c532279b316147
yazid69 <yazidlembirik69@outlook.fr>
2024-11-06 16:11:59 +0100

    Account

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\RapperController;
+use App\Http\Controllers\UserController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -12,4 +13,6 @@ Route::get('/rapper/{id}', [RapperController::class, 'getRapper']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
+    Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'getUser']);
+    Route::middleware('auth:sanctum')->post('/user/change-password', [UserController::class, 'changePassword']);
 });
